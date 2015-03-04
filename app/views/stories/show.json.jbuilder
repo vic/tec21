0b5a5fde986f94a98ffa225c8725a1af1eeec802
Victor Borja <vborja@apache.org>
2015-03-04 19:24:08 +0000

    Add stories via scaffold

---
+++ app/views/stories/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @story, :id, :content, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :stories
+
   root 'welcome#index'
   
   controller :welcome do
